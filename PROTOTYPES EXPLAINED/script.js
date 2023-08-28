ab0f0ef9fab830b4ea454145652b7a1a72288a1e
ben64103 <benabolade@gmail.com>
2023-08-28 07:50:06 +0100

    added JSON

--- PROTOTYPES EXPLAINED/script.js
+++ PROTOTYPES EXPLAINED/script.js
@@ -1,15 +1,29 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthday = new Date(dob)
-  this.calculateAge = function() {
-    const diff = Date.now() - this.birthday.getTime()
-    const ageDate = new Date(diff)
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.birthday = new Date(dob)
+//   this.calculateAge = function() {
+//     const diff = Date.now() - this.birthday.getTime()
+//     const ageDate = new Date(diff)
+//     return Math.abs(ageDate.getUTCFullYear() - 1970)
+//   }
+// }
+
+// const john = new Person('John', 'Doe', '8-12-90')
+// const mary = new Person('Mary', 'Johnson', 'March 20 1978')
+
+// console.log(mary)
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob)
+
+    this.calculateAge = function() {
+      const difference = Date.now() - this.birthday.getTime()
+      const ageDate = new Date(difference)
+      return Math.abs(ageDate.getUTCFullYear() - 1970)
+    }
   }
 }
-
-const john = new Person('John', 'Doe', '8-12-90')
-const mary = new Person('Mary', 'Johnson', 'March 20 1978')
-
-console.log(mary)
