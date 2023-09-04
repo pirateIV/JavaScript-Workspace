a54a919cbe3ad403c197748bc2a24e62b894a155
ben64103 <benabolade@gmail.com>
2023-09-04 08:02:46 -0700

    added sets

---
+++ SETs/script.js
@@ -0,0 +1,17 @@
+// SETS - store unique values of any type
+
+const set1 = new Set()
+
+// add Values to the set
+
+set1.add(100)
+set1.add(100)
+set1.add(100)
+set1.add(50)
+
+console.log(set1)
+
+if(set1.has(10) )  console.log(true)
+ else  console.log(false)
+
+// const names = { 'benjamin','' }
