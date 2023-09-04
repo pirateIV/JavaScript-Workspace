87eb424f8f7198f72a252723a59d51a4dcf3f3bd
ben64103 <benabolade@gmail.com>
2023-09-03 18:49:57 -0700

    graph for technical interviews

--- BIG O/graph/script.js
+++ BIG O/graph/script.js
@@ -11,3 +11,26 @@
 // 2. Breadth First Traversal = Queue
   // add from the back and remove from the front
 
+const depthFirstPrint = ( graph, sourceNode ) => {
+  const stack = [ sourceNode ] ;
+
+  while ( stack.length > 0 ) {
+    const current = stack.pop()
+    console.log(current)
+    for ( let neighbour of  graph[current] ) {
+      stack.push( neighbour )
+    }
+  }
+};
+
+
+const graph = {
+  a: ['b', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: ['f'],
+  e: [],
+  f: []
+}
+
+depthFirstPrint(graph, 'a') // abdfce
